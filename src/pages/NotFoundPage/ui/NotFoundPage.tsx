1f210577d7a35f55a133355a0ce0245bebb18da3
Petrov Ilya <dnamix1@gmail.com>
2023-05-30 16:58:11 +0300

    proper not-found page

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,29 @@
+import { FC } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import moduleClasses from './NotFoundPage.module.scss';
+
+import NotFoundPageImg from '../../../shared/assets/not_found.jpg';
+
+interface NotFoundPageProps {
+    className?: string;
+}
+
+export const NotFoundPage: FC<NotFoundPageProps> = ({ className }) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(moduleClasses.notFoundPage, {}, [className])}>
+            <div className={moduleClasses.backdrop}>
+                <img
+                    src={NotFoundPageImg}
+                    alt={t('page not found')}
+                />
+                <h1>
+                    {t('This Page does not exist')}
+                </h1>
+            </div>
+        </div>
+
+    );
+};
--- src/shared/config/routerConfig/routerConfig.tsx
+++ src/shared/config/routerConfig/routerConfig.tsx
@@ -1,17 +1,20 @@
 import { RouteProps } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage';
 import { AboutPage } from 'pages/AboutPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 
 export enum AppRoutes {
     MAIN = 'main',
-    ABOUT = 'about'
+    ABOUT = 'about',
+    NOT_FOUND = 'not-found'
 }
 
 export const RouterPaths: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]:
-            '/',
+        '/',
     [AppRoutes.ABOUT]:
-            '/about',
+        '/about',
+    [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routerConfig: Record<AppRoutes, RouteProps> = {
@@ -22,5 +25,9 @@ export const routerConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RouterPaths.about,
         element: <AboutPage />,
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RouterPaths['not-found'],
+        element: <NotFoundPage />,
     },
 };
